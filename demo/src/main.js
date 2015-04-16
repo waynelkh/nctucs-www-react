630f88c1afc7fbf3b919806869f2f6e2752ed20a
wnlee <wnlee@cs.nctu.edu.tw>
2015-04-17 06:50:58 +0800

    Talk about state

--- demo/src/main.js
+++ demo/src/main.js
@@ -3,11 +3,32 @@ var Header = require('./header.js');
 
 var main = React.createClass({
 
+    getInitialState: function() {
+        return {
+            number: 0,
+            like: true
+        };
+    },
+
+    handleClick: function(){
+        this.setState({
+            number: this.state.number+1,
+            like: !this.state.like
+        });
+    },
+
     render: function() {
+        var isLike = this.state.like ? 'glyphicon glyphicon-heart' : '';
         return (
             <div>
                 <Header />
-                hello world
+                <p>Number: {this.state.number}</p>
+                <p>
+                    Like or not: <spen className={isLike}></spen>
+                </p>
+                <button onClick={this.handleClick}>Push</button>
+
+
             </div>
         );
     }
