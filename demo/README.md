cbb1e5976c5b13241eef7e544bde4ef7a6fd355b
wnlee <wnlee@cs.nctu.edu.tw>
2015-04-17 08:06:21 +0800

    Add webpack-dev-server and chagne jsx to babel

---
+++ demo/README.md
@@ -0,0 +1,9 @@
+### To run this example
+
+```
+npm install
+npm run build
+npm start
+
+open browser http://localhost:8080/
+```
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,13 +1,40 @@
+var Path = require('path');
+var webpack =require('webpack');
+
 module.exports = {
-    entry: "./src/boot.js",
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        Path.resolve(__dirname, 'src/boot.js')
+    ],
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: Path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: 'style!css' },
-            { test: /\.js$/, loader: 'jsx?harmony'},
+        {
+            test: /\.js$/,
+            loaders: ['react-hot', 'babel'],
+            include: Path.join(__dirname, 'src/')
+        },
+        {
+            test: /\.css$/, loader: 'style!css'
+        },
+        {
+            test: /\.less$/, loader: 'style!css!less'
+        },
+        {
+          test: /\.(html|png)$/,
+          loader: "file?name=[path][name].[ext]&context=./src"
+        },
+        {
+          test: /\.(ttf|eot|png|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "url-loader?limit=8192"
+        }
         ]
-    }
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin()
+    ]
 };
